b436beaa5d9e4ac6f0fd76a84295ec8792ea972a
lang-sky <langsky121@gmail.com>
2021-03-06 11:24:01 -0500

    hot frontend loading, babel, webpack, ssr

---
+++ webpack.server.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  entry: './server/index.js',
+
+  target: 'node',
+
+  externals: [nodeExternals()],
+
+  output: {
+    path: path.resolve('server-build'),
+    filename: 'index.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        // exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ['css-loader'],
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
